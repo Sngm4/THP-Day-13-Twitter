aec11fd620aa35dd1a7579cb9d240656bd321842
Iris <marlx42@protonmail.com>
2022-04-20 10:14:27 +0200

    commit 1

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source "https://rubygems.org"
 ruby '2.7.4'
+gem 'pry'
 gem 'dotenv'
+gem 'twitter'
--- lib/app.rb
+++ lib/app.rb
@@ -1,10 +1,26 @@
-require 'dotenv'# Appelle la gem Dotenv
+# Gem required to run the program
+require 'dotenv'
+require 'twitter' 
+require 'pry'
 
-Dotenv.load('.env') # Ceci appelle le fichier .env (situé dans le même dossier que celui d'où tu exécute app.rb)
-# et grâce à la gem Dotenv, on importe toutes les données enregistrées dans un hash ENV
+# For call the '.env' 
+Dotenv.load('.env') 
 
-# Il est ensuite très facile d'appeler les données du hash ENV, par exemple là je vais afficher le contenu de la clé TWITTER_API_SECRET
-puts ENV['TWITTER_API_SECRET']
+# Few lines for call API Keys from '.env'
+def login_twitter()
+  client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+    config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+    config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+    config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+  end
+  return client
+end
 
-#Autre exemple 
-puts ENV['BEST_WEBSITE_EVER']
+# The Tweet !
+def the_tweet(login_twitter)
+  client = client.update('Mon premier tweet en Ruby !!!!')
+  return client
+end
+
+the_tweet(login_twitter)
